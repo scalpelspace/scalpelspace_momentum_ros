645bc760abb33227240c1d7ed165f352c7c175cc
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-07-19 19:52:55 -0400

    Update CAN dbc gen code

--- include/scalpelspace_momentum_ros/momentum_driver/momentum_can_dbc.h
+++ include/scalpelspace_momentum_ros/momentum_driver/momentum_can_dbc.h
@@ -1,11 +1,31 @@
-/** Auto-generated CAN message definitions from DBC file. */
+/*******************************************************************************
+ * @file momentum_can_dbc.h
+ * @brief Auto-generated CAN message definitions from DBC file.
+ *******************************************************************************
+ */
 
 #ifndef MOMENTUM_CAN_DBC_H
 #define MOMENTUM_CAN_DBC_H
 
+/** CPP guard open. ***********************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Includes. *****************************************************************/
+
 #include "momentum_can_driver.h"
+
+/** Public variables. *********************************************************/
 
 extern const can_message_t dbc_messages[];
 extern const int dbc_message_count;
+
+/** CPP guard close. **********************************************************/
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MOMENTUM_CAN_DBC_H
