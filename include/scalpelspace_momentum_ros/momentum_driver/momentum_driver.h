6a18534646c273d0bae9c3df1d12ff8a1baaeddb
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-07-19 18:30:46 -0400

    Add initial CAN drivers

---
+++ include/scalpelspace_momentum_ros/momentum_driver/momentum_driver.h
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * @file momentum_driver.h
+ * @brief Momentum driver for SPI and CAN based communication.
+ *******************************************************************************
+ */
+
+#ifndef MOMENTUM__DRIVER_H
+#define MOMENTUM__DRIVER_H
+
+/** Includes. *****************************************************************/
+
+#include "momentum_can_dbc.h"
+#include "momentum_can_driver.h"
+#include "momentum_spi_driver.h"
+
+#endif
